1fa0a312bbe1cfd9daca5c7be765b6417c17ca99
jeffkrantz <jjkrantz@gmail.com>
2017-07-05 14:18:37 -0500

    post

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -29,3 +29,722 @@ $('#delete').on('click', function(){
     }
   })
 })
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+v
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+v
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
+
+
+
+$('#edit-button').on('click', function(){
+  $('#title-text').hide();
+  $('#title-edit').show();
+})
+
+var postId = window.location.pathname.split('/')[3]
+
+$('#save-button').on('click', function(){
+  var input = $('#title-input').val();
+
+  $.ajax({
+    method: 'PATCH',
+    url: '/posts/' + postId,
+    data: {title: input},
+    success: function(response){
+      $('#title-text').show();
+      $('#title-edit').hide();
+      $('#title').text(response.title);
+    }
+  })
+})
+
+$('#delete').on('click', function(){
+  $.ajax({
+    method: 'DELETE',
+    url: '/posts/' + postId,
+    success: function(response){
+      window.location = '/posts/all'
+    }
+  })
+})
